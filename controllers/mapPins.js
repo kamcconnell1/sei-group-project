367cdf42948eda6f5f625ada7900d60e84703967
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-05-18 04:21:33 +0100

    all errors handled. rearranged backend. pins get needs to be done

--- controllers/mapPins.js
+++ controllers/mapPins.js
@@ -1,90 +1,81 @@
-const MapPin = require('../models/mapPin')
-const { notFound, unauthorized } = require('../lib/errorMessages')
+const User = require('../models/user')
+const { notFound } = require('../lib/errorMessages')
 
 
-//* function to get all pins of a user
-//! WORKING tested
-//! ERROR not tested 
-async function mapPinsIndex(req, res, next) {
-  req.body.user = req.currentUser
-  try {
-    const mapPins = await MapPin.find()
-    if (!mapPins) throw new Error(notFound)
-    console.log(mapPins[0].user)
-    if (!mapPins[0].user.equals(req.currentUser._id)) throw new Error(unauthorized)
-    res.status(200).json(mapPins)
-  } catch (err) {
-    next(err)
-  }
-}
-
-//* function to create a map pin
-//! WORKING tested
-//! ERROR not tested 
+// //? Function to create a Map Pin
+//* WORKING tested
+//* ERROR tested
 async function mapPinsCreate(req, res, next) {
   req.body.user = req.currentUser
   try {
-    const createdMapPin = await MapPin.create(req.body)
-    res.status(201).json(createdMapPin)
+    const user = await User.findById(req.currentUser)
+    user.pins.push(req.body)
+    user.save()
+    res.status(201).json(user.pins)
   } catch (err) {
     next(err)
   }
 }
 
-//* function to get single map pin
-//! WORKING tested
-//! ERROR not tested 
+//? Function to get Single Map Pin
+//* WORKING tested
+//* ERROR tested 
 async function mapPinShow(req, res, next) {
   req.body.user = req.currentUser
   try {
-    const mapPinId = req.params.id
-    const mapPin = await MapPin.findById(mapPinId)
-    if (!mapPinId) throw new Error('Not Found')
-    if (!mapPin.user.equals(req.currentUser._id)) throw new Error(unauthorized)
-    res.status(200).json(mapPin)
+    const pinId = req.params.pinId
+    console.log(pinId)
+    const user = await User.findById(req.currentUser)
+    const mapped = user.pins.id(pinId)
+    console.log(mapped)
+    if (!mapped) throw new Error(notFound)
+    res.status(200).json(mapped)
   } catch (err) {
     next(err)
   }
 }
 
 
-//* function to update a map pin
-//! WORKING tested
-//! ERROR not tested 
+//? Function to Update a Map Pin
+//* WORKING tested
+//* ERROR tested
 async function mapPinUpdate(req, res, next) {
   req.body.user = req.currentUser
   try {
-    const mapPin = await MapPin.findById(req.params.id)
-    if (!mapPin) throw new Error(notFound)
-    if (!mapPin.user.equals(req.currentUser._id)) throw new Error(unauthorized)
-    Object.assign(mapPin, req.body)
-    await mapPin.save()
-    res.status(202).json(mapPin)
+    const pinId = req.params.pinId
+    console.log(pinId)
+    const user = await User.findById(req.currentUser)
+    const pinToUpdate = user.pins.id(pinId)
+    if (!pinToUpdate) throw new Error(notFound)
+    console.log(pinToUpdate) 
+    Object.assign(pinToUpdate, req.body)
+    await user.save()
+    res.status(202).json(pinToUpdate)
   } catch (err) {
     next(err)
   }
 }
 
 
-//* function to delete a map pin
-//! WORKING tested
-//! ERROR not tested 
+//? Function to Delete a Map Pin
+//* WORKING tested
+//* ERROR tested
 async function mapPinDelete(req, res, next) {
   try {
-    const mapPinId = req.params.id
-    const mapPinToDelete = await MapPin.findById(mapPinId)
-    if (!mapPinToDelete) throw new Error(notFound)
-    if (!mapPinToDelete.user.equals(req.currentUser._id)) throw new Error('Unauthorized')
-    await mapPinToDelete.remove()
+    const pinId = req.params.pinId
+    const user = await User.findById(req.currentUser)
+    const pinDelete = user.pins.id(pinId)
+    if (!user.pins.includes(pinDelete)) throw new Error(notFound)
+    await pinDelete.remove()
+    await user.save()
     res.sendStatus(204)
   } catch (err) {
     next(err)
   }
 }
 
-
 module.exports = {
-  index: mapPinsIndex,
+  // index: getPins,
   create: mapPinsCreate,
   single: mapPinShow,
   update: mapPinUpdate,
