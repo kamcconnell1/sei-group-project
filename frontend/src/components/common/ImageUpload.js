a18b4f147fd5935b02df8ad77604d4a66388fc2d
Katie <kamcconnell1@gmail.com>
2020-05-18 06:59:40 +0100

    added some styling to profile page

--- frontend/src/components/common/ImageUpload.js
+++ frontend/src/components/common/ImageUpload.js
@@ -31,7 +31,6 @@ class ImageUpload extends React.Component{
       
       render() {
         const { image } = this.state
-        console.log(this.props);
         
     return (
       <>
--- frontend/src/components/users/UserClothCard.js
+++ frontend/src/components/users/UserClothCard.js
@@ -4,19 +4,17 @@ import {Link} from 'react-router-dom'
 
 const UserClothCard = ({_id, title, image, rentalPrice}) => {
   return (
-    <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+    <div className="column is-half">
     <Link to={`/clothes/${_id}`}>
       <div className="card">
-        <div className="card-header">
-          <h4 className="card-header-title">{title}</h4>
-        </div>
         <div className="card-image">
           <figure className="image image is-1by1">
             <img src={image[0]} alt={title} loading="lazy" width="255" height="255" />
           </figure>
         </div>
         <div className="card-content">
-          <h5 className="">{`£${rentalPrice}`}</h5>
+          <p className="title is-5">{title}</p>
+          <p className="subtitle is-6">{`Rent £${rentalPrice} (7 Days)`}</p>
         </div>
       </div>
     </Link>
