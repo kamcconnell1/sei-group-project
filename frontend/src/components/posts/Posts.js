b3e7110655e1efd1e3699ee67a76fd49814060f7
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-05-19 17:49:38 +0100

    posts index added

--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -10,33 +10,41 @@ class Posts extends React.Component {
     posts: null,
   }
 
-async componentDidMount () {
-  try {
-    const res = await getAllPosts()
-    console.log(res.data)
-    this.setState({ posts: res.data})
-  } catch (err) {
-    console.log(err)
+  async componentDidMount() {
+    try {
+      const res = await getAllPosts()
+      console.log(res.data)
+      this.setState({ posts: res.data })
+    } catch (err) {
+      console.log(err)
+    }
   }
-}
 
 
-render() {
-  if (!this.state.tracks) return null
-  return (
-    <>
-    <h1>s</h1>
-    <div>
-    {this.state.posts.map(post => (
-      <PostCards
-      key={post._id}
-      {...this.state.posts}
-      />
-    ))}
-    </div>
-    </>
-  )
-}
+  render() {
+    if (!this.state.posts) return null
+    return (
+      <>
+        <section className="hero is-light">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns is-multiline">
+                <h1>Posts</h1>
+                <div>
+                  {this.state.posts.map(post => (
+                    <PostCards
+                      key={post._id}
+                      {...post}
+                    />
+                  ))}
+                </div>
+              </div>
+              </div>
+            </div>
+        </section>
+      </>
+    )
+  }
 }
 
 export default Posts
--- frontend/src/components/posts/PostsCards.js
+++ frontend/src/components/posts/PostsCards.js
@@ -2,21 +2,22 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-const PostCards = ({}) => {
+const PostCards = ({ photo, text, user, title, _id}) => {
   return (
     <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
-    <Link to={`/posts/`}>
+    <Link to={`/posts/${_id}`}>
       <div className="card">
         <div className="card-header">
+          <h2>{title}</h2>
         </div>
         <div className="card-image">
           <figure className="image image is-1by1">
-            {/* <img src={} alt={} loading="lazy" width="255" height="255" /> */}
+            <img src={photo} alt={title} loading="lazy" width="255" height="255" />
           </figure>
         </div>
         <div className="card-content">
-          <h4 className=""><strong>{}</strong></h4>
-          <h5 className=""><strong>Rental Price:</strong> {`s`}</h5>
+          <h4 className=""><strong>{text}</strong></h4>
+          <h5 className=""><strong>Posted by</strong> {user.username}</h5>
         </div>
       </div>
     </Link>
