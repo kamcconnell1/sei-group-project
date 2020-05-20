adccb7023d2f2d50a952703f8d0ad8dbf963accb
bmbmb <bmbmb@me.com>
2020-05-20 19:41:30 +0100

    styling my-profile and other bits

--- frontend/src/components/users/EditProfile.js
+++ frontend/src/components/users/EditProfile.js
@@ -1,77 +1,31 @@
 import React from 'react'
 
-import { avatarArr } from '../common/Images'
-import { uploadClothesImage } from '../../lib/ext_api'
-import ImageUpload from '../common/ImageUpload'
-
-const EditProfile = ({ onChange, onSubmit, toggleModal, modalOpen }) => {
-
+const EditProfile = ({ onChangeEdit, onSubmitEdit, toggleModalEdit, modalOpenEdit, username, errors }) => {
   return (
-
     <div
-      className={modalOpen ? "modal is-active" : "modal"}
+      className={modalOpenEdit ? "modal is-active" : "modal"}
     >
       <div className="modal-background"></div>
 
       <div className="edit-profile-form">
         <form
           className="box"
-          onSubmit={onSubmit}
+          onSubmit={onSubmitEdit}
         >
           <div className="modal-content">
-
-            <div className="field">
-              {/* <div className="is-profile-picture is-centered is-primary">
-            Select a New Avatar
-          </div>
-          <div className="columns is-multiline ">
-            <div className="image is-96x96" >
-              <img
-                name="profilePic"
-                src={avatarArr[0]} alt="avatar1"
-                onClick={console.log('clicked avatar')
-                }
-              />
-            </div>
-          </div> */}
-              <div className="image is128x128">
-                <ImageUpload
-                  onChange={onChange}
-                  preset={uploadClothesImage}
-                  name="profilePic"
-                  labelText="Or Upload Picture"
+            <div className="field" >
+              <label className="label">Username</label>
+              <div className="control">
+                <input
+                  className={`input ${errors ? 'is-danger' : '' }`}
+                  type="text"
+                  placeholder="Enter Username here"
+                  name="username"
+                  value={username}
+                  onChange={onChangeEdit}
                 />
               </div>
             </div>
-
-            {/* <div className="field">
-                <label className="label">Username</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Enter Username here"
-                    name="username"
-                    value={username}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Enter email here"
-                    name="email"
-                    value={email}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div> */}
-
             <div className="field">
               <button type="submit"
                 className="button is-fullwidth is-primary">
@@ -79,12 +33,10 @@ const EditProfile = ({ onChange, onSubmit, toggleModal, modalOpen }) => {
           </button>
             </div>
           </div >
-
-
         </form >
         <button
           className="modal-close is-large"
-          onClick={toggleModal}
+          onClick={toggleModalEdit}
           aria-label="close"></button>
       </div>
     </div>
--- frontend/src/components/users/UserProfile.js
+++ frontend/src/components/users/UserProfile.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import UserClothCard from './UserClothCard'
+import MessageCard from './MessageCard'
+import EditProfilePicture from './EditProfilePicture'
 import EditProfile from './EditProfile'
-import { getProfile, editProfile, deleteProfile } from '../../lib/api'
+import { getProfile, editProfile, deleteProfile, inboxMessage, getUserProfile } from '../../lib/api'
 import { logout } from '../../lib/auth'
 import { getPostcodeInfo } from '../../lib/ext_api'
 import Comments from '../common/Comments'
 import StarRating from '../common/StarRating'
+
 // ! User profile, GETs data for user on mount
 class UserProfile extends React.Component {
   state = {
@@ -15,26 +18,46 @@ class UserProfile extends React.Component {
     latitude: '',
     longitude: '',
     modalOpen: false,
+    modalOpenEdit: false,
     commentsArray: [],
-    rating: 0
+    rating: 0,
+    errors: {},
+    messages: null
   }
   // * Function to GET the users details
   async componentDidMount() {
     try {
       await this.getUserDashboard()
+      await this.getInbox()
+      this.interval = setInterval(this.getInbox, 120000)
     } catch (err) {
       console.log(err)
     }
   }
+
+
   async getUserDashboard() {
     try {
       const res = await getProfile()
-      this.setState({ user: res.data, commentsArray: res.data.comments })
+      this.setState({ user: res.data, commentsArray: res.data.comments, errors: '' })
       this.getLocation()
     } catch (err) {
       console.log(err)
     }
   }
+
+  // * Function to GET incoming messages
+  async getInbox() {
+    try {
+      const res = await inboxMessage()
+      this.setState({ messages: res.data })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+
   //* Function to find user location details
   async getLocation() {
     try {
@@ -69,7 +92,7 @@ class UserProfile extends React.Component {
       this.getUserDashboard()
       console.log('submit event res', res)
     } catch (err) {
-      console.log(err.response.data);
+      console.log(err.response.data)
     }
   }
   //* Function to toggle state to show the image upload form or not
@@ -77,12 +100,34 @@ class UserProfile extends React.Component {
     this.setState({ modalOpen: !this.state.modalOpen })
   }
 
+  toggleModalEdit = () => {
+    this.setState({ modalOpenEdit: !this.state.modalOpenEdit })
+  }
+
+  handleChangeEdit = (e) => {
+    const user = { ...this.state.user, [e.target.name]: e.target.value }
+    this.setState({ user })
+  }
+
+  handleSubmitEdit = async e => {
+    e.preventDefault()
+    try {
+      console.log('presub', this.state.user)
+      const res = await editProfile(this.state.user)
+      console.log(res.data)
+      this.toggleModalEdit()
+      this.getUserDashboard()
+    } catch (err) {
+      this.setState({ errors: 'username' })
+    }
+  }
+
   //* Delete Profile
   deleteUserProfile = async e => {
-    try{
-    await deleteProfile()
-    await logout()
-    this.props.history.push(`/`)
+    try {
+      await deleteProfile()
+      await logout()
+      this.props.history.push(`/`)
     } catch (err) {
       console.log(err)
     }
@@ -96,54 +141,103 @@ class UserProfile extends React.Component {
   onStarClick = () => {
     console.log('clicked')
   }
-  
+
   render() {
-    if (!this.state.user || !this.state.location) return null
-    // consts taken from state to populate user data shown on the page
-    const { username, createdArticles, profilePic } = this.state.user
-    const { commentsArray } = this.state
-    const location = this.state.location
-    console.log('state is', this.state.user);
-    return (
-      <>
-        <div className="Page-head">
-          <div className="Page-title">
-            <h1>My Profile</h1>
-          </div>
-          {/* <div className="Page-subtitle">
-            <h2>USERNAME?</h2>
-          </div> */}
-        </div>
-        <section className="section">
-          <div className="container">
-            <div className="columns">
-              <div className="column is-3 is-profile-info">
-                {/* Section for avatar or profile pic need to change to allow to change the file  & so appears over the form appears over the avatar on hover */}
+      if (!this.state.user || !this.state.location || !this.state.messages) return null
+      // consts taken from state to populate user data shown on the page
+      const { username, createdArticles, profilePic } = this.state.user
+      const { commentsArray, messages } = this.state
+      const location = this.state.location
+      const reversedCreatedArticles = createdArticles.reverse().slice(0, 6)
+      console.log(messages[0].createdAt)
+      return (
+        <>
+
+          <div className="My-profile">
+
+            <div className="My-profile-top-row">
+
+              {/* Section for avatar or profile pic need to change to allow to change the file  & so appears over the form appears over the avatar on hover */}
+              <div className="Photo-delete-rating">
                 <div className="profile-img image is-128x128">
                   <img src={profilePic} alt="profile pic" />
                   <button onClick={this.toggleModal}
                     className="button is-profile-btn"
                   >Change Profile Picture</button>
                 </div>
-                <EditProfile
+                <EditProfilePicture
                   toggleModal={this.toggleModal}
                   modalOpen={this.state.modalOpen}
                   onChange={this.handleChange}
                   onSubmit={this.handleSubmit}
                 />
                 {/* Section for the user details - username, location & star rating. button to add clothes to profile   */}
+                <button onClick={this.toggleModalEdit}
+                  className="button is-profile-btn"
+                >Update Info</button>
+                <EditProfile
+                  errors={this.state.errors}
+                  state={this.state.user}
+                  toggleModalEdit={this.toggleModalEdit}
+                  modalOpenEdit={this.state.modalOpenEdit}
+                  onChangeEdit={this.handleChangeEdit}
+                  onSubmitEdit={this.handleSubmitEdit}
+                > <button>Edit</button> </EditProfile>
+                <button onClick={() => { if (window.confirm("Are you sure?")) this.deleteUserProfile() }} className="button is-danger">Delete</button>
+                {/* //! NEED TO ADD STAR RATINGS HERE  */}
+
                 <div className="control">
-                  <h5 className="title">Welcome {username}</h5>
-                  <h6 className="subtitle">{location}</h6>
-                  <button onClick={() => {if (window.confirm("Are you sure?")) this.deleteUserProfile()}} className="button is-danger">Delete</button>
-                  {/* //! NEED TO ADD STAR RATINGS HERE  */}
                   <p>Star Rating</p>
                   <StarRating
                     onStarClick={this.onStarClick}
                     rating={this.state.rating}
                   />
-                  <hr />
+                  <button onClick={() => { if (window.confirm("Are you sure?")) this.deleteUserProfile() }} className="button is-danger">Delete</button>
                 </div>
+              </div>
+              <div className="Welcome">
+                <h5 className="title">Welcome {username}</h5>
+                <h6 className="subtitle">{location}</h6>
+              </div>
+            </div>
+            <div className="My-profile-columns">
+              <div className="Left-col">
+                <h1>left column</h1>
+                <h1>left column</h1>
+                <h1>left column</h1>
+                <h1>left column</h1>
+              </div>
+
+              <div className="Center-col">
+                {/* Map over the clothes the user has uploaded - need to work on the positioning of this - need to add to allow user to edit / delete items */}
+                <div className="My-items">
+                  <div className="My-items-title">
+                    <h2>My Items</h2>
+                    <hr />
+                  </div>
+
+                  <div>
+                    {/* Ternary with text showing if no articles been created yet  */}
+                    {(reversedCreatedArticles.length === 0) ?
+                      <div className="">
+                        <h1>Looks like you haven't uploaded anthing yet.</h1>
+                        <p> Why don't you add some clothes now? <br /> Or browse the clothes that are on offer? </p>
+                      </div>
+                      :
+                      <div className="My-items-index">
+                        {reversedCreatedArticles.map(item =>
+                          <UserClothCard
+                            {...item}
+                            key={item._id}
+                          />
+                        )}
+                      </div>
+                    }
+                  </div>
+                </div>
+              </div>
+
+              <div className="Right-col">
                 <button className="button is fullwidth"
                   onClick={this.handleAddClothes}
                 >Add Clothes Now</button>
@@ -153,52 +247,44 @@ class UserProfile extends React.Component {
                 </div>
                 <hr />
                 <div>
-                  <Link to={`/profile/${username}/favourites`} className="button">Favourite</Link>
+                  <Link to={`/profile/${username}/favourites`} className="button">Favourite Clothes</Link>
                 </div>
-              </div>
-              {/* Map over the clothes the user has uploaded - need to work on the positioning of this - need to add to allow user to edit / delete items */}
-              <div className="column is-multiline is-user-clothes">
-                <div className="users-articles has-text-centered">
-                  <hr />
-                  <h2>YOUR ITMES</h2>
-                  <hr />
-                  {/* Ternary with text showing if no articles been created yet  */}
-                  {(createdArticles.length === 0) ?
-                    <div className="container">
-                      <h1>Looks like you haven't uploaded anthing yet.</h1>
-                      <p> Why don't you add some clothes now? <br /> Or browse the clothes that are on offer? </p>
-                    </div>
-                    :
-                    <div className="control">
-                      {createdArticles.map(item =>
-                        <UserClothCard
-                          {...item}
-                          key={item._id}
-                        />
-                      )}
-                    </div>
-                  }
+                <hr />
+                <div>
+                  <Link to={`/profile/${username}/favouriteposts`} className="button">Favourite Posts</Link>
                 </div>
+
+
+                {/* Notifications / chat section */}
+                <div className="column is-3 is-user-chat">
+                  Messages
+                  <div>
+                    {messages.map(message =>
+                      <MessageCard
+                        key={message._id}
+                        {...message}
+                      />
+                    )}
+                  </div>
+                </div>
+
+                <section>
+                  <div>
+                    {commentsArray.map(comment => (
+                      <Comments
+                        key={comment._id}
+                        comment={comment}
+                      />
+                    ))}
+                  </div>
+                </section>
               </div>
-              {/* Notifications / chat section */}
-              <div className="column is-3 is-user-chat">
-                Incoming Notifications
-          </div>
             </div>
+
           </div>
-        </section>
-        <section>
-          <div>
-            {commentsArray.map(comment => (
-              <Comments
-                key={comment._id}
-                comment={comment}
-              />
-            ))}
-          </div>
-        </section>
-      </>
-    )
+
+        </>
+      )
+    }
   }
-}
-export default UserProfile
+  export default UserProfile
