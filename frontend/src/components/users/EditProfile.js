b9bcb5fe0edb7f4491a8de54b6540b5901bb866c
Katie <kamcconnell1@gmail.com>
2020-05-21 15:53:25 +0100

    merge conflcit

--- frontend/src/components/users/EditProfile.js
+++ frontend/src/components/users/EditProfile.js
@@ -4,49 +4,6 @@ import EditClothCard from '../users/EditClothCard'
 import { editProfile, getProfile, deleteCloth } from '../../lib/api'
 import { deletedItemToast } from '../../lib/toasts'
 
-<<<<<<< HEAD
-const EditProfile = ({ onChangeEdit, onSubmitEdit, toggleModalEdit, modalOpenEdit, username, errors }) => {
-  return (
-    <div
-      className={modalOpenEdit ? "modal is-active" : "modal"}
-    >
-      
-      <div className="Edit-profile-form">
-      <div className="modal-background"></div>
-        <form
-          className="box"
-          onSubmit={onSubmitEdit}
-        >
-          <div className="modal-content">
-            <div className="field" >
-              <label className="label">Username</label>
-              <div className="control">
-                <input
-                  className={`input ${errors ? 'is-danger' : '' }`}
-                  type="text"
-                  placeholder="Enter Username here"
-                  name="username"
-                  value={username}
-                  onChange={onChangeEdit}
-                />
-              </div>
-            </div>
-            <div className="field">
-              <button type="submit"
-                className="button is-fullwidth is-primary">
-                Update Profile
-          </button>
-            </div>
-          </div >
-        </form >
-        <button
-          className="modal-close is-large"
-          onClick={toggleModalEdit}
-          aria-label="close"></button>
-      </div>
-    </div>
-  )
-=======
 class EditProfile extends React.Component {
   state = {
     user: '',
@@ -127,7 +84,6 @@ deleteArticle = async (_id) => {
    </div>
     )
   }
->>>>>>> development
 }
 
 
