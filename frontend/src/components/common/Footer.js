cb0e9a18a6d5ad2422dcd28fd4bd37deeae1f77d
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-05-22 11:03:44 +0100

    footer sorted

--- frontend/src/components/common/Footer.js
+++ frontend/src/components/common/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="Footer">
       <div className="content has-text-centered">
         <p>
           <strong>KEBB</strong> by <a href="https://jgthms.com">Katie</a>, <a href="https://jgthms.com">Edward</a>, <a href="https://jgthms.com">Benga</a>, <a href="https://jgthms.com">Blanca</a> GA Group Project.
